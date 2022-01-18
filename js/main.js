576f9f4303698dc29a306735da838b4031036b99
Snezhana <snezhana.bilonoga16@gmail.com>
2022-01-19 00:30:13 +0300

    remove comments

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
 import App from "./App.js"
-import NotesAPI from "./NotesAPI.js";
 
 const root = document.getElementById("app");
-
-// NotesAPI.saveNote({
-//     name: 'kek3',
-//     category: 'ololo',
-//     content: "pppppp",
-//     dates: ['3/5/2021', '5/5/2021'],
-// });
-
-const app = new App(root);
+new App(root);
